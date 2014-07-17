e2b959342b29aedc982153f55b7538b737559e9c
Ramsundar Shandilya <ram.shandilya@ymedialabs.com>
2014-07-17 16:06:24 +0530

    Initial Commit

---
+++ FlickrPhotos/RSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RSAppDelegate.h
+//  FlickrPhotos
+//
+//  Created by Ramsundar Shandilya on 17/07/14.
+//  Copyright (c) 2014 Ramsundar Shandilya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlickrPhotosTests/FlickrPhotosTests.m
@@ -0,0 +1,34 @@
+//
+//  FlickrPhotosTests.m
+//  FlickrPhotosTests
+//
+//  Created by Ramsundar Shandilya on 17/07/14.
+//  Copyright (c) 2014 Ramsundar Shandilya. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FlickrPhotosTests : XCTestCase
+
+@end
+
+@implementation FlickrPhotosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
